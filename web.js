61d2b89c6465f2dfe8f3cb6d6af08f33acd9125d
Kevin London <kevinlondon@gmail.com>
2013-07-19 04:23:02 +0000

    Uses input from index.html

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var contents = fs.readFileSync('index.html');
+  response.send(contents);
 });
 
 var port = process.env.PORT || 5000;
