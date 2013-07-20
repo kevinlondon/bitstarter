30c20a00dd37f16968e10136bfa3ad4af761782a
Kevin London <kevinlondon@gmail.com>
2013-07-20 06:03:52 +0000

    Almost there. Accepts url as input

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@
 var fs = require("fs");
 var program = require("commander");
 var cheerio = require("cheerio");
+var rest = require("restler");
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = "";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,7 +38,24 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
+var assertUrlExists = function(url) {
+    rest.get(url).on('complete', function(result) {
+        if(result instanceof Error) {
+            console.log("%s does not exist. Exiting.", url);
+            process.exit(1);
+        } else {
+            return result;
+        }
+    });
+    return url;
+};
+
 var cheerioHtmlFile = function(htmlfile) {
+    if(URL_DEFAULT != "penguins") {
+        console.log("URL: " + URL_DEFAULT);
+    } else {
+        console.log("Bummer.")
+    }
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
@@ -46,6 +65,12 @@ var loadChecks = function(checksfile) {
 
 var checkHtmlFile = function(htmlfile, checksfile) {
     $ = cheerioHtmlFile(htmlfile);
+    var out = runChecks($, checksfile);
+    return out;
+};
+
+var runChecks = function(source, checksfile) {
+    $ = source;
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -65,8 +90,19 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <link>', 'Path to investigatable url.', assertUrlExists)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
+
+    var checkJson = "";
+    if(program.url){
+        rest.get(program.url).on('complete', function(data) {
+            $ = cheerio.load(data);
+            checkJson = runChecks($, program.checks);
+            console.log(checkJson);
+        });
+    } else {
+        var checkJson = checkHtmlFile(program.file, program.checks);
+    }
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
 } else {
